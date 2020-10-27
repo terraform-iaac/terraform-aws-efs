5b767f922df9f9b4ff33aed87835dcba82fdd72a
Dmytro Ostapenko <d.ostapenko97@gmail.com>
2020-10-27 11:29:17 +0200

    Add ARN to outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -4,6 +4,9 @@ output "efs_id" {
 output "efs_endpoint" {
   value = aws_efs_file_system.efs_storage.dns_name
 }
+output "efs_arn" {
+  value = aws_efs_file_system.efs_storage.arn
+}
 output "sg_id" {
   value = module.nfs_sg.id
 }
